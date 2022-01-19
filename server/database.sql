7f82e0c6bd54b1b37f1ed78ac417a18fbbe77340
albertlai431 <albertlai631@outlook.com>
2022-01-19 10:56:29 -0500

    fix sql command

--- server/database.sql
+++ server/database.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE inventory;
-
+\c inventory;
 CREATE TABLE items(
     item_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
     amount INTEGER NOT NULL,
-    price NUMERIC(2) NOT NULL
+    price NUMERIC(10,2) NOT NULL
 );
